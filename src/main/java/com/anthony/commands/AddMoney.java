ba2bf7f5f49ede3c30f7914c7cbb0d06f8d77b0a
PattonsPCS <patton64@pnw.edu>
2025-04-29 12:30:05 -0500

    Skibidi stuff 5

--- src/main/java/com/anthony/commands/AddMoney.java
+++ src/main/java/com/anthony/commands/AddMoney.java
@@ -6,13 +6,14 @@ import io.papermc.paper.command.brigadier.BasicCommand;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
 @NullMarked
 @SuppressWarnings("UnstableApiUsage")
-public class AddMoney implements BasicCommand{
+public class AddMoney implements BasicCommand {
     private final Econ econ;
 
     public AddMoney(Econ econ) {
@@ -21,37 +22,26 @@ public class AddMoney implements BasicCommand{
 
     @Override
     public void execute(CommandSourceStack source, String[] args) {
-        if (!(source.getExecutor() instanceof Player)) {
-            if (source.getExecutor() != null) {
-                source.getExecutor().sendMessage("You must be an operator to use this command.");
-            }
-
-            if(args.length < 2) {
-                Component errorMessage = Component.text("Usage: /addmoney <player> <amount>").color(NamedTextColor.RED);
-                if(source.getExecutor() != null) {
-                    source.getExecutor().sendMessage(errorMessage);
-                }
-                return;
-            }
-
-            String playerName = args[0];
-            int amount = Integer.parseInt(args[1]);
-            Player player = econ.getServer().getPlayer(playerName);
-            if(source.getExecutor() != null){
-                if(player == null) {
-                    source.getExecutor().sendMessage("Player not found.");
-                    return;
-                }
-                econ.getAccount(player).deposit(amount);
-                source.getExecutor().sendMessage(Component.text("You have added " + amount + " to your account.")
-                        .color(NamedTextColor.GREEN));
-            }
+        CommandSender sender = source.getSender();
+        if (args.length != 2) {
+            sender.sendRichMessage("<red>Usage: /addmoney <player> <amount>");
+            return;
         }
+
+        String playerName = args[0];
+        int amount = Integer.parseInt(args[1]);
+        Player player = Bukkit.getPlayerExact(playerName);
+        if (player == null) {
+            source.getSender().sendRichMessage("<red>Player not found.");
+            return;
+        }
+        econ.getAccount(player).deposit(amount);
+        source.getExecutor().sendRichMessage("<gold>You have added <yellow>" + amount + "</yellow> to your account.</gold>");
     }
 
 
     @Override
-    public boolean canUse(CommandSender sender){
+    public boolean canUse(CommandSender sender) {
         return sender.hasPermission("econplugin.addmoney.use");
     }
 }

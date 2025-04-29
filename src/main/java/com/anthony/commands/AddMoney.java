1b3efd3b199d3ecb3190f650e183f79b7c19186e
PattonsPCS <patton64@pnw.edu>
2025-04-29 12:35:10 -0500

    Skibidi Toilet Bitch

--- src/main/java/com/anthony/commands/AddMoney.java
+++ src/main/java/com/anthony/commands/AddMoney.java
@@ -9,7 +9,11 @@ import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @NullMarked
 @SuppressWarnings("UnstableApiUsage")
@@ -32,13 +36,22 @@ public class AddMoney implements BasicCommand {
         int amount = Integer.parseInt(args[1]);
         Player player = Bukkit.getPlayerExact(playerName);
         if (player == null) {
-            source.getSender().sendRichMessage("<red>Player not found.");
+            sender.sendRichMessage("<red>Player not found.");
             return;
         }
         econ.getAccount(player).deposit(amount);
-        source.getExecutor().sendRichMessage("<gold>You have added <yellow>" + amount + "</yellow> to your account.</gold>");
+        sender.sendRichMessage("<gold>You have added <yellow>" + amount + "</yellow> to your account.</gold>");
     }
 
+    public Collection<String> suggest(final CommandSourceStack commandSourceStack, final String[] args) {
+        if (args.length == 1) {
+          return Bukkit.getOnlinePlayers().stream()
+              .map(Player::getName)
+              .filter(name -> name.startsWith(args[0]))
+              .toList();
+        }
+        return Collections.emptyList();
+    }
 
     @Override
     public boolean canUse(CommandSender sender) {

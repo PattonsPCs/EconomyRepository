3462c7eee2a24fa0e9fdd26ad0e1a8902e767dee
PattonsPCS <patton64@pnw.edu>
2025-05-01 11:03:17 -0500

    Action bar!

--- src/main/java/com/anthony/events/MobKillListener.java
+++ src/main/java/com/anthony/events/MobKillListener.java
@@ -1,5 +1,6 @@
 package com.anthony.events;
 
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,6 +26,6 @@ public class MobKillListener implements Listener {
         int currentBalance = econData.getBalance(uuid);
         int reward = 15;
         econData.setBalance(uuid, currentBalance + reward);
-        killer.sendRichMessage("<bold><italic><dark_gray>You killed a <white>" + event.getEntity().getName() + "</white>. +$" + reward + "</dark_gray></bold>");
+        killer.sendActionBar(MiniMessage.miniMessage().deserialize("<bold><italic><dark_gray>You killed a <white>" + event.getEntity().getName() + "</white>. +$" + reward + "</dark_gray></bold>"));
     }
 }

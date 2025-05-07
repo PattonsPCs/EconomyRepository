19ba9301d868134ed88325aff79496e07ae40003
PattonsPCS <patton64@pnw.edu>
2025-05-07 13:25:18 -0500

    Changing multiple things + trying more advanced Brigadier commands

--- src/main/java/com/anthony/Econ.java
+++ src/main/java/com/anthony/Econ.java
@@ -1,33 +1,38 @@
 package com.anthony;
 
-import com.anthony.commands.*;
+import com.anthony.commands.EconomyCommand;
 import com.anthony.configuration.ShopConfig;
-import com.anthony.events.MobKillListener;
-import com.anthony.events.TransactionListener;
 import com.anthony.persistence.EconPersistenceManager;
-import com.anthony.transactions.PluginLogger;
+import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.IOException;
 
 
+
 @SuppressWarnings("UnstableApiUsage")
 public class Econ extends JavaPlugin {
   @Getter
   private ShopConfig shopConfig;
-  private EconData econData;
-  private EconPersistenceManager persistenceManager;
-  private final PluginLogger econLogger = new PluginLogger(this, "receipts.txt");
+    private EconPersistenceManager persistenceManager;
 
 
   @Override
   public void onEnable() {
     getLogger().info("EconPlugin is starting...");
-    econData = new EconData();
+      EconData econData = new EconData();
+    EconomyCommand econCommand = new EconomyCommand(econData);
     persistenceManager = new EconPersistenceManager(econData, this);
     shopConfig = new ShopConfig(this);
     shopConfig.load();
+
+    this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, event -> {
+      Commands commandRegistrar = event.registrar();
+
+      commandRegistrar.register(econCommand.build());
+    });
+
 
 
     try{
@@ -42,19 +47,14 @@ public class Econ extends JavaPlugin {
     }
 
 
-    getServer().getPluginManager().registerEvents(new MobKillListener(econData, econLogger), this);
-    getServer().getPluginManager().registerEvents(new TransactionListener(econLogger), this);
-    this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS,
-        event -> {
-          event.registrar().register("buy", "Buy something from the shop.", new BuyCommand(this, econData));
-          event.registrar().register("balance", "Check your balance.", new BalanceCommand(econData));
-          event.registrar().register("addmoney", "Add money to an account", new EconomyCommand(econData));
-          event.registrar().register("shop", "See items in the shop", new ShopCommand(this));
-          event.registrar().register("save", "Saves balances", new SaveCommand(persistenceManager));
-        });
+    this.getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, commands -> {
+    });
+
     getLogger().info("EconPlugin has been enabled!");
     shopConfig.reload();
   }
+
+
 
 
   @Override
--- src/main/java/com/anthony/commands/EconomyCommand.java
+++ src/main/java/com/anthony/commands/EconomyCommand.java
@@ -4,27 +4,22 @@ package com.anthony.commands;
 import com.anthony.EconData;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import io.papermc.paper.command.brigadier.BasicCommand;
+import com.mojang.brigadier.tree.LiteralCommandNode;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.resolvers.selector.PlayerSelectorArgumentResolver;
-import org.bukkit.Bukkit;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.jspecify.annotations.NullMarked;
-
-import java.util.Collection;
-import java.util.Collections;
-import java.util.UUID;
 
 @SuppressWarnings("UnstableApiUsage")
 public class EconomyCommand  {
     private EconData econData;
+    private int defaultBalance;
 
 
     public EconomyCommand(EconData econData) {
         this.econData = econData;
+        this.defaultBalance = 100;
     }
 
     LiteralArgumentBuilder<CommandSourceStack> root = Commands.literal("economy")
@@ -32,7 +27,7 @@ public class EconomyCommand  {
                     .then(Commands.argument("target", ArgumentTypes.player())
                             .then(Commands.argument("amount", IntegerArgumentType.integer())
                                     .executes(ctx -> {
-                                        PlayerSelectorArgumentResolver targetResolver = (PlayerSelectorArgumentResolver) ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
+                                        PlayerSelectorArgumentResolver targetResolver =  ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
                                         Player target = targetResolver.resolve(ctx.getSource()).getFirst();
 
                                         int amount = IntegerArgumentType.getInteger(ctx, "amount");
@@ -43,7 +38,57 @@ public class EconomyCommand  {
                     )
             )
             .then(Commands.literal("remove")
-                    .then)
+                    .then(Commands.argument("target", ArgumentTypes.player())
+                            .then(Commands.argument("amount", IntegerArgumentType.integer())
+                                    .executes(ctx -> {
+                                        PlayerSelectorArgumentResolver targetResolver =  ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
+                                        Player target = targetResolver.resolve(ctx.getSource()).getFirst();
+                                        int amount = IntegerArgumentType.getInteger(ctx, "amount");
+                                        econData.withdraw(target.getUniqueId(), amount);
+                                        return amount;
+                                    })
+                            )
+                    )
+            )
+            .then(Commands.literal("view")
+                    .then(Commands.argument("target", ArgumentTypes.player())
+                            .executes(ctx -> {
+                                PlayerSelectorArgumentResolver targetResolver =  ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
+                                Player target = targetResolver.resolve(ctx.getSource()).getFirst();
+
+                                return econData.getBalance(target.getUniqueId());
+                            })
+                    )
+            )
+            .then(Commands.literal("set")
+                    .then(Commands.argument("target", ArgumentTypes.player())
+                            .then(Commands.argument("amount", IntegerArgumentType.integer())
+                                    .executes(ctx -> {
+                                        PlayerSelectorArgumentResolver targetResolver =  ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
+                                        Player target = targetResolver.resolve(ctx.getSource()).getFirst();
+                                        int amount = IntegerArgumentType.getInteger(ctx, "amount");
+                                        econData.setBalance(target.getUniqueId(), amount);
+                                        return econData.getBalance(target.getUniqueId());
+                                    })
+                            )
+                    )
+            )
+            .then(Commands.literal("reset")
+                    .then(Commands.argument("target", ArgumentTypes.player())
+                            .executes(ctx -> {
+                                PlayerSelectorArgumentResolver targetResolver =  ctx.getArgument("target", PlayerSelectorArgumentResolver.class);
+                                Player target = targetResolver.resolve(ctx.getSource()).getFirst();
+
+                                int amount = defaultBalance;
+                                econData.setBalance(target.getUniqueId(), amount);
+                                return econData.getBalance(target.getUniqueId());
+                            })
+                    )
+            );
+
+    public LiteralCommandNode<CommandSourceStack> build(){
+        return root.build();
+    }
 
 }
 
